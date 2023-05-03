1fd38e5b64ef67415b2234850d12a8a97241e4bd
Zong77 <113457197+Zong77@users.noreply.github.com>
2023-05-03 15:16:46 +0800

    Update Hierarchical Clustering.py

--- Hierarchical Clustering.py
+++ Hierarchical Clustering.py
@@ -10,8 +10,8 @@ from scipy.cluster.hierarchy import linkage, dendrogram
 from matplotlib import pyplot as plt
 
 X = [[0, 0], [1, 1], [2, 1], [1, 3], [2, 4], [3, 3]]
-Z = linkage(X, "centroid")
-fig = plt.figure(figsize = (5, 3))
+Z = linkage(X, "centroid") #將X矩陣傳入"linkage"函數中，使用"centroid"方法計算矩陣中所有點的距離
+fig = plt.figure(figsize = (5, 3)) #創建一個大小為(5, 3)的新圖形，存放樹狀圖
 dendrogram(Z)
 
 plt.show()
